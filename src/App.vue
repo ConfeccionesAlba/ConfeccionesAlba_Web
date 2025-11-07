b2a325b99aa54f3e995ccc88da0c7fc6fe8a7993
alex6dj <alexeiagueroalba@gmail.com>
2025-11-07 13:39:02 -0300

    feat(layout): add basic header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Header from "@/components/layout/Header.vue";
+import Footer from "@/components/layout/Footer.vue";
 </script>
 
 <template>
-  <RouterView />
+  <Header/>
+  <RouterView/>
+  <Footer/>
 </template>
 
 <style scoped>
