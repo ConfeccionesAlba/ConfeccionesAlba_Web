c72db5d8e39a602b7d86bf666d6ea00b88d15c2e
alex6dj <alexeiagueroalba@gmail.com>
2025-11-07 14:09:40 -0300

    refactor(ui): remove vuestic ui library

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,26 +1,8 @@
 <script setup>
-
-import {VaNavbar, VaNavbarItem} from "vuestic-ui";
 </script>
 
 <template>
-  <VaNavbar>
-    <template #left>
-      <VaNavbarItem class="logo">
-        <router-link to="/">
-          <img width="32" src="@/assets/sewing_machine.svg" alt="">
-        </router-link>
-      </VaNavbarItem>
-    </template>
-    <template #center>
-      <VaNavbarItem class="hidden sm:block">
-        <router-link to="/">Inicio</router-link>
-      </VaNavbarItem>
-      <VaNavbarItem class="hidden sm:block">
-        <router-link to="/products">Productos</router-link>
-      </VaNavbarItem>
-    </template>
-  </VaNavbar>
+
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
-import {createVuestic} from "vuestic-ui"
 import "vuestic-ui/css"
 import "material-design-icons-iconfont/dist/material-design-icons.min.css"
 
@@ -8,24 +7,6 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createVuestic({
-    config: {
-        colors: {
-            variables: {
-                // Default colors
-                primary: "#23e066",
-                secondary: "#002c85",
-                success: "#40e583",
-                info: "#2c82e0",
-                danger: "#e34b4a",
-                warning: "#ffc200",
-                gray: "#babfc2",
-                dark: "#34495e",
-            },
-        },
-    },
-}))
 app.use(createPinia())
 app.use(router)
 
