5dee7942e1c8d03cc4a8b3b6ceddb2c4cbafcb26
alex6dj <alexeiagueroalba@gmail.com>
2025-11-07 14:25:16 -0300

    refactor(ui): remove unused stylesheet imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
-import "vuestic-ui/css"
-import "material-design-icons-iconfont/dist/material-design-icons.min.css"
 
 import App from './App.vue'
 import router from './router'
