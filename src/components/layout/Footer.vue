31381c4fefb62e30341fe7880e01f3bccd892d00
alex6dj <alexeiagueroalba@gmail.com>
2025-11-07 15:45:04 -0300

    feat(ui): add basic footer with copyright
    Implements a simple footer component for the application layout, displaying a copyright notice and a link to the author's profile.

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-
+  <div>
+    <div class="footer text-center mx-auto mt-4 p-3 bg-opacity-10 text-success bg-success rounded-3"> &copy; Hecho con
+      <i class="bi bi-heart-fill"></i>
+      por
+      <a href="https://www.linkedin.com/in/alex6dj" target="_blank" rel="noopener noreferrer"> alex6dj</a>
+    </div>
+  </div>
 </template>
 
 <style scoped>
