f08c61c75c9de07d061270ed744d0026014be7b5
alex6dj <alexeiagueroalba@gmail.com>
2025-12-04 18:58:45 -0300

    feat(api): add product image upload functionality
    - Add setImageByProductId method to productsService
    - Remove file field from IProductCreate and IProductUpdate interfaces
    - Add IUploadImageResponse interface for image upload responses
    - Update createProduct and updateProductById methods to handle API responses properly
    
    BREAKING CHANGE: The file field has been removed from IProductCreate and IProductUpdate interfaces. Image uploads should now be handled separately using the new setImageByProductId method.

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,7 +1,12 @@
 import api from '@/services/api'
 import type IApiResponse from '@/types/apiResponse'
 import { API_ENDPOINT_NAMES } from '@/constants/apiNames'
-import type { IProduct, IProductCreate, IProductUpdate } from '@/types/productsResponse.ts'
+import type {
+  IProduct,
+  IProductCreate,
+  IProductUpdate,
+  IUploadImageResponse,
+} from '@/types/productsResponse.ts'
 
 export default {
   async getProducts() {
@@ -35,13 +40,11 @@ export default {
     }
   },
 
-  // TODO: get file and upload as multipart/form-data
   async createProduct(newProduct: IProductCreate) {
     try {
       const response = await api.post<IApiResponse<IProduct>>(
         API_ENDPOINT_NAMES.PRODUCTS,
         newProduct,
-        { headers: { 'Content-Type': 'multipart/form-data' } },
       )
 
       if (response.data.isSuccess) {
@@ -55,13 +58,10 @@ export default {
     }
   },
 
-  // TODO: get file and upload as multipart/form-data
   async updateProductById(id: number, productUpdate: IProductUpdate) {
     try {
       const endpoint = `${API_ENDPOINT_NAMES.PRODUCTS}/${id}`
-      const response = await api.put<IApiResponse<IProduct>>(endpoint, productUpdate, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      })
+      const response = await api.put<IApiResponse<IProduct>>(endpoint, productUpdate, {})
 
       if (response.data.isSuccess) {
         return
@@ -70,6 +70,25 @@ export default {
       }
     } catch (err) {
       console.error('Product update failed', err)
+      throw err
+    }
+  },
+
+  // TODO: get file and upload as multipart/form-data
+  async setImageByProductId(id: number, file: File) {
+    try {
+      const endpoint = `${API_ENDPOINT_NAMES.PRODUCTS}/${id}/images`
+      const response = await api.put<IApiResponse<IUploadImageResponse>>(endpoint, file, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+
+      if (response.data.isSuccess) {
+        return response.data.result
+      } else {
+        throw new Error(response.data.errorMessages.join('\r\n'))
+      }
+    } catch (err) {
+      console.error('Image update failed', err)
       throw err
     }
   },
